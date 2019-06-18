b1455a3ea89b4813e2bf8b3a6fa3790749d05f82
amitsinkataria <amit_kataria10@yahoo.co.in>
2019-06-18 16:48:08 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,35 @@
+
+function myReset(){
+    location.reload()
+    }
+
+ 
+function but2() {
+var x = document.getElementById("buu");
+if (x.innerHTML === "v") {
+  x.innerHTML = ">";
+} else {
+  x.innerHTML = "v";
+}
+}
+
+function but() {
+var x = document.getElementById("buu2");
+if (x.innerHTML === "v") {
+  x.innerHTML = ">";
+} else {
+  x.innerHTML = "v";
+}
+
+
+}
+
+
+
+function myFunction() {
+document.getElementById("myForm").submit();
+}
+
+function closed(){
+  FormData.close();
+}
